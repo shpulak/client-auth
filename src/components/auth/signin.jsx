1ae9e925639c2169ea5b9352d63d857b61d68a94
Shashwat <shashwat.pulak@gmail.com>
2018-01-21 11:37:58 +0530

    Connect Sigin request with reducer and local storage of auth token

---
+++ src/components/auth/signin.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signin extends Component {
+    constructor(props){
+        super(props);
+    }
+    handleFormSubmit({email, password}) {
+        // console.log(email, password);
+        console.log(email, password);
+        this.props.signinUser({email,password});
+    }
+    render() {
+        const { handleSubmit, fields: { email, password } } = this.props;
+        return (
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <fieldset className="form-group">
+                    <label>Email:</label>
+                    <input {...email} className="form-control" />
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Password:</label>
+                    <input {...password} className="form-control" />
+                </fieldset>
+                <button action="submit" className="btn btn-primary">Sign In</button>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'signin',
+    fields: ['email', 'password']
+}, null, actions)(Signin);
