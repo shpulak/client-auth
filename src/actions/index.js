063ac42fbcc365e77e32e72b347247b6e41c9098
Shashwat <shashwat.pulak@gmail.com>
2018-01-21 23:06:54 +0530

    Add signup page with handling of network errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -9,13 +9,40 @@ export function signinUser({ email, password }) {
         axios.post(`${ROOT_URL}/signin`, { email, password })
             .then(response => {
                 dispatch({
-                    type:AUTH_USER
+                    type: AUTH_USER
                 })
-                localStorage.setItem('token',response.data.token);
+                localStorage.setItem('token', response.data.token);
                 browserHistory.push('/feature');
             })
             .catch(() => {
-
+                dispatch(authError('Bad Login Info!'))
             })
     };
+}
+
+export function authError(error) {
+    return {
+        type: AUTH_ERROR,
+        payload: error
+    }
+}
+
+export function signoutUser() {
+    localStorage.removeItem('token');
+    return { type: UNAUTH_USER }
+}
+
+export function signupUser({email, password}){
+    return function(dispatch){
+        axios.post(`${ROOT_URL}/signup`,{email,password})
+        .then(response => {
+            dispatch({
+                type: AUTH_USER
+            })
+            localStorage.setItem('token', response.data.token);
+            browserHistory.push('/feature');
+        }).catch(({response}) => {
+            dispatch(authError(response.data.error))
+        })
+    }
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/app.jsx';
 import Signin from './components/auth/signin.jsx';
+import Signup from './components/auth/signup.jsx';
+import Signout from './components/auth/signout.jsx';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -16,6 +18,8 @@ ReactDOM.render(
     <Router history={browserHistory}>
       <Route path='/' component={App} >
         <Route path='/signin' component={Signin} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/signout' component={Signout} />
       </Route>
     </Router>
   </Provider>
